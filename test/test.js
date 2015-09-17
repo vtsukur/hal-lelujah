c1934d8fabc94f9618893980f85412cce54abbcb
vtsukur <flushdia@gmail.com>
2015-09-18 00:47:01 +0300

    Renaming resource() to representation(), to express the intent more precisely.

--- test/test.js
+++ test/test.js
@@ -6,16 +6,16 @@ var hal = require('../');
 
 describe('hal-lelujah', function() {
 
-    describe('resource', function() {
+    describe('representation', function() {
 
         it('builds empty representation out of empty data object', function() {
-            var representation = hal.resource({});
+            var representation = hal.representation({});
             //noinspection JSUnresolvedVariable
             expect(representation).to.eql({});
         });
 
         it('builds representation out of non-empty data object where result matches data object', function() {
-            var representation = hal.resource({ key: 'value' });
+            var representation = hal.representation({ key: 'value' });
             //noinspection JSUnresolvedVariable
             expect(representation).to.eql({ key: 'value' });
         });
