d7a3497338cf453c8ca62dbf54b0ebf4db829179
vtsukur <flushdia@gmail.com>
2015-09-18 00:45:26 +0300

    Adding resource() functionality which is able to build representation out of non-empty data object where result matches data object. Adding missing semicolons. Aligning naming of the tests.

--- test/test.js
+++ test/test.js
@@ -8,11 +8,17 @@ describe('hal-lelujah', function() {
 
     describe('resource', function() {
 
-        it('builds empty representation out of empty data', function() {
+        it('builds empty representation out of empty data object', function() {
             var representation = hal.resource({});
             //noinspection JSUnresolvedVariable
             expect(representation).to.eql({});
-        })
+        });
+
+        it('builds representation out of non-empty data object where result matches data object', function() {
+            var representation = hal.resource({ key: 'value' });
+            //noinspection JSUnresolvedVariable
+            expect(representation).to.eql({ key: 'value' });
+        });
 
     });
 
