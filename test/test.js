7981b1781b7f3ca7f176fc5a7aff4ef6e3589b28
vtsukur <flushdia@gmail.com>
2015-10-11 01:19:51 +0300

    Adding elaborate test for mapping verification.

--- test/test.js
+++ test/test.js
@@ -15,11 +15,28 @@ describe('hal-lelujah', function() {
             expect(repr).to.eql({});
         });
 
-        it('builds representation out of non-empty data object where result matches data object', function() {
-            var repr = hal.representation({ key: 'value' });
+        it('builds representation out of non-empty data object using direct mapping from the data object', function() {
+            var source = {
+                boolean: true,
+                number: 7,
+                string: 'property',
+                object: {
+                    key: 'value'
+                },
+                array: [
+                    true,
+                    7,
+                    'property',
+                    {
+                        key: 'value'
+                    },
+                    [ ]
+                ]
+            };
+            var repr = hal.representation(source);
 
             //noinspection JSUnresolvedVariable
-            expect(repr).to.eql({ key: 'value' });
+            expect(repr).to.eql(source);
         });
 
     });
