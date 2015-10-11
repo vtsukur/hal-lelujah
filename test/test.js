233b641034aeb213deb468de93d115c3f53eabbb
glvtsuku <volodymyr.tsukur@avid.com>
2015-10-11 15:42:31 +0300

    Converting null and undefined data objects into an empty data object.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var dataDriven = require('data-driven');
 
 var hal = require('../');
 
@@ -8,11 +9,17 @@ describe('hal-lelujah', function() {
 
     describe('representation', function() {
 
-        it('builds empty representation out of empty data object', function () {
-            var repr = hal.representation({});
+        dataDriven([
+          { value: undefined, description: 'undefined' },
+          { value: null, description: 'null' },
+          { value: {}, description: 'empty' }
+        ], function() {
+            it('builds empty representation out of {description} data object', function(context) {
+                var repr = hal.representation(context.value);
 
-            //noinspection JSUnresolvedVariable
-            expect(repr.toJSON()).to.eql({});
+                //noinspection JSUnresolvedVariable
+                expect(repr.toJSON()).to.eql({});
+            });
         });
 
         it('builds representation out of non-empty data object using direct mapping from the data object', function() {
