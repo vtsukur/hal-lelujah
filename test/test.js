7fd247fd38d833755cbeab373234f73f565c64df
vtsukur <flushdia@gmail.com>
2015-10-11 15:43:24 +0300

    Applying 2 spaces indentation.

--- test/test.js
+++ test/test.js
@@ -5,47 +5,47 @@ var dataDriven = require('data-driven');
 
 var hal = require('../');
 
-describe('hal-lelujah', function() {
+describe('hal-lelujah', function () {
 
-    describe('representation', function() {
+  describe('representation', function () {
 
-        dataDriven([
-          { value: undefined, description: 'undefined' },
-          { value: null, description: 'null' },
-          { value: {}, description: 'empty' }
-        ], function() {
-            it('builds empty representation out of {description} data object', function(context) {
-                var repr = hal.representation(context.value);
+    dataDriven([
+      {value: undefined, description: 'undefined'},
+      {value: null, description: 'null'},
+      {value: {}, description: 'empty'}
+    ], function () {
+      it('builds empty representation out of {description} data object', function (context) {
+        var repr = hal.representation(context.value);
 
-                //noinspection JSUnresolvedVariable
-                expect(repr.toJSON()).to.eql({});
-            });
-        });
-
-        it('builds representation out of non-empty data object using direct mapping from the data object', function() {
-            var source = {
-                boolean: true,
-                number: 7,
-                string: 'property',
-                object: {
-                    key: 'value'
-                },
-                array: [
-                    true,
-                    7,
-                    'property',
-                    {
-                        key: 'value'
-                    },
-                    [ ]
-                ]
-            };
-            var repr = hal.representation(source);
-
-            //noinspection JSUnresolvedVariable
-            expect(repr.toJSON()).to.eql(source);
-        });
-
+        //noinspection JSUnresolvedVariable
+        expect(repr.toJSON()).to.eql({});
+      });
     });
+
+    it('builds representation out of non-empty data object using direct mapping from the data object', function () {
+      var source = {
+        boolean: true,
+        number: 7,
+        string: 'property',
+        object: {
+          key: 'value'
+        },
+        array: [
+          true,
+          7,
+          'property',
+          {
+            key: 'value'
+          },
+          []
+        ]
+      };
+      var repr = hal.representation(source);
+
+      //noinspection JSUnresolvedVariable
+      expect(repr.toJSON()).to.eql(source);
+    });
+
+  });
 
 });
